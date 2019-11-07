dd3cff05790ce8ea3d6f5d0f98eb2c72265d7334
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-11-06 19:02:38 -0500

    chore: 1.1.1.
    Updates the version from 1.1.0 to 1.1.1.

--- src/webpack_loader_remote/__init__.py
+++ src/webpack_loader_remote/__init__.py
@@ -3,4 +3,4 @@ default_app_config = "webpack_loader_remote.apps.WebpackLoaderRemoteConfig"
 # Do not change this version manually.
 # Versioning is managed by package_controller.
 # To update the version run `pc version --patch | --minor | --major`
-__version__ = "1.1.0"
+__version__ = "1.1.1"
